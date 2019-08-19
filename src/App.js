d04f87aebc323f602b55c028818c132d3a46ee2d
SirLime <jook11@ukr.net>
2019-08-19 20:07:38 +0300

    Add apollo-fetch and test it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+const { createApolloFetch } = require('apollo-fetch');
 
 function App() {
+  const [launchInfo, setLaunchInfo] = useState({});
+
+  useEffect(() => {
+    const apolloFetch = createApolloFetch({
+      uri: 'https://api.spacex.land/graphql/'
+    });
+
+    apolloFetch({
+      query: `query UpcomingLaunche {
+      launchesUpcoming(sort: "launch_date_local", limit: 1) {
+        launch_date_local
+        rocket {
+          rocket {
+            name
+          }
+        }
+      }
+    }`,
+      variables: { id: 1 }
+    }).then(res => setLaunchInfo(res.data.launchesUpcoming[0]));
+  }, []);
+
+  useEffect(() => console.log(JSON.stringify(launchInfo, null, '\t')), [
+    launchInfo
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SpaceX</h1>
+      <h2>
+        rocket:{' '}
+        {Object.keys(launchInfo).length > 0 && launchInfo.rocket.rocket.name}
+      </h2>
     </div>
   );
 }
-
 export default App;
