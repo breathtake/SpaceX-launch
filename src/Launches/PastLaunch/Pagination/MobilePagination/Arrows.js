a0c21a39c43e2c23b4116664da5546d7fcb8c336
breathtake <jook11@ukr.net>
2020-03-12 15:13:53 +0200

    Fix nav arrows

--- src/Launches/PastLaunch/Pagination/MobilePagination/Arrows.js
+++ src/Launches/PastLaunch/Pagination/MobilePagination/Arrows.js
@@ -9,7 +9,7 @@ const BackIcon = (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="rgba(255, 255, 255, 0.75)"
+    fill="rgba(0, 0, 0, 0.75)"
   >
     <path d="M0 0h24v24H0z" fill="none" />
     <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
@@ -22,7 +22,7 @@ const ForwardIcon = (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="rgba(255, 255, 255, 0.75)"
+    fill="rgba(0, 0, 0, 0.75)"
   >
     <path d="M0 0h24v24H0z" fill="none" />
     <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
